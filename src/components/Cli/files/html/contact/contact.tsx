7020525b5c53e1d52143d2235191c72f21f354ff
Nico Ismaili <nico.ismaili@aoe.com>
2023-12-27 02:15:49 +0100

    feat: contact links via cms

--- src/components/Cli/files/html/contact/contact.tsx
+++ src/components/Cli/files/html/contact/contact.tsx
@@ -1,48 +1,55 @@
 import { CliFile } from '@/components/Cli/files/CliFile';
 import { Link } from '@/components/Link';
-import { FaGithub, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
-import { IoIosMail } from 'react-icons/io';
+import { graphql, useStaticQuery } from 'gatsby';
+
+const ContactRun = () => {
+  const data = useStaticQuery(graphql`
+    query GetContactLinks {
+      allStrapiContactLink {
+        nodes {
+          icon_link {
+            text
+            url
+            svgHtml
+          }
+        }
+      }
+    }
+  `);
+
+  const iconClasses = `
+    inline-block
+    mr-2
+    w-5
+    h-5
+    `;
+  return (
+    <div className="flex flex-col justify-start gap-1">
+      {data.allStrapiContactLink.nodes.map((node: any, i: number) => {
+        const { svgHtml, text, url } = node.icon_link;
+        return (
+          <div key={`${text}-${i}`} className="flex items-center">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: svgHtml,
+              }}
+              className={iconClasses}
+            />
+            <Link href={url}>{text}</Link>
+          </div>
+        );
+      })}
+      <span className="mt-2.5">© Nico Ismaili, 2023</span>
+    </div>
+  );
+};
+
 export class Contact extends CliFile {
   get fileName(): string {
     return `contact.html`;
   }
 
   public run() {
-    const iconClasses = `
-  inline
-    mr-2
-    w-5
-    h-5
-    `;
-    return (
-      <div className="flex flex-col justify-start gap-1">
-        <span>
-          <IoIosMail className={iconClasses} />
-          <Link href="mailto:nico@ismaili.de">nico@ismaili.de</Link>
-        </span>
-
-        <span>
-          <FaLinkedin className={iconClasses} />
-          <Link href="https://linkedin.com/in/ismailinico">ismailinico</Link>
-        </span>
-
-        <span>
-          <FaGithub className={iconClasses} />
-          <Link href="https://github.com/nico-i">nico-i</Link>
-        </span>
-
-        <span>
-          <FaInstagram className={iconClasses} />
-          <Link href="https://instagram.com/nico.ismaili">nico.ismaili</Link>
-        </span>
-
-        <span>
-          <FaYoutube className={iconClasses} />
-          <Link href="https://youtube.com/channel/nico-i">Nico I.</Link>
-        </span>
-
-        <span className="mt-2.5">© Nico Ismaili, 2023</span>
-      </div>
-    );
+    return <ContactRun />;
   }
 }

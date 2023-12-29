fff61990c4cd023c9145fc3b022bc77e57c682ff
Nico Ismaili <nico.ismaili@aoe.com>
2023-12-29 22:42:13 +0100

    fix: build

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars -- necessary var to allow JSX
 const React = require(`react`);
-const Layout = require(`./src/components/layout`).default;
+const Layout = require(`./src/components/Layout`).Layout;
 require(`./src/styles/global.css`);
 
 exports.wrapPageElement = ({ element, props }) => {

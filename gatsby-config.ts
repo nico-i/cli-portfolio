4b6768a1b4754f73030df1651c69eef5feb944f5
Nico Ismaili <nico.ismaili@aoe.com>
2023-12-29 22:50:35 +0100

    fix: only use dotenv during dev

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,10 @@
 import type { GatsbyConfig } from 'gatsby';
-require(`dotenv`).config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+
+if (process.env.NODE_ENV === `development`) {
+  require(`dotenv`).config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+}
 
 const path = require(`path`);
 
